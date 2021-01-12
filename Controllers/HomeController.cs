101000d2a48114392c4fbacc7a6c25bf61c33690
petersond <petersond@APCCWO190056LP>
2021-01-12 12:12:26 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BizSteam.Models;
+using Newtonsoft.Json;
+
+namespace BizSteam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult CreateForm(ContactFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", viewModel);
+            }
+            var destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Results.txt");
+            System.IO.File.WriteAllText(destPath, JsonConvert.SerializeObject(viewModel));
+            
+            ViewBag.Message = "Successfully Added";
+            return View("Index");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ContactFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BizSteam.Models
+{
+    public class ContactFormViewModel
+    {
+        [DisplayName("First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+    public class FormValidator : AbstractValidator<ContactFormViewModel>
+    {
+        public FormValidator()
+        {
+            RuleFor(x => x.Email).Must((model, mail) =>
+            {
+                var emailAddressAttribute = new EmailAddressAttribute();
+                return emailAddressAttribute.IsValid(mail);
+            }).WithMessage("Please enter a valid email");
+        }
+    }
+}
